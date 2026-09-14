9c35c9c8c2ed5a8e447c8274388ddfb1faff19fa
khasanislamov1-cmyk <khasan.islamov1@gmail.com>
2026-09-14 20:19:36 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mbcares-app-v1";
+const CACHE = "mbcares-app-v2";
 const SHELL = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
